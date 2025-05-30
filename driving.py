6655e2782abf6e829c30a14bbf72df25811bdf1a
duckyboi22 <showadamori02@gmail.com>
2025-05-30 18:28:36 +1200

    made repeated code into functions, made minor tweaks

--- driving.py
+++ driving.py
@@ -1,5 +1,5 @@
 """
-    A SQL and python program that can:
+A SQL and python program that can:
 - List countries based on road rules with an advanced filtering and ordering
 - Search for a specific country
 - Compare two countries based on their road rules
@@ -30,6 +30,10 @@ except ImportError:
 connection = sqlite3.connect("country.db")
 cursor = connection.cursor()
 
+# changeable lists
+last_updated = "29th of May 2025"  # last updated date for statistics (UPDATE MANUALLY)
+country_columns = "country.country_code, country.country_name, country.left_side, country.min_licence_age, traffic_rules.rule_name, traffic_rules.rule_value"  # list without IDs
+loading_time = 0.1
 
 # clear screen
 def clear():
@@ -38,7 +42,7 @@ def clear():
 
 # check if order input is valid
 def order_check(order_choice):
-    valid_order = ["I", "N", "M", "D", "A", "S"]
+    valid_order = ["N", "M", "D", "A", "S"]
     return order_choice in valid_order
 
 
@@ -74,6 +78,24 @@ def choice_check(choice_input):
     return choice_input in valid_choice
 
 
+# title display
+def display_header(title):
+    clear()
+    print("="*70)
+    print("\033[1m{:^70}\033[0m".format(title))
+    print("="*70)
+
+
+# header text (without id)
+def get_header():
+    return ['\033[1mCOUNTRY CODE\033[0m', '\033[1mNAME\033[0m', '\033[1mDRIVING SIDE\033[0m', '\033[1mMIN DRIVING AGE\033[0m', '\033[1mRULE NAME\033[0m', '\033[1mRULE VALUE\033[0m']
+
+
+# formatted row text (without id)
+def get_format(data):
+    return [(code, name, 'Left' if side else 'Right', age, rule_name, 'None' if rule_value == -1 else rule_value) for code, name, side, age, rule_name, rule_value in data]
+
+
 while True:
     # clear terminal and ask for what they want to do
     clear()
@@ -100,11 +122,7 @@ while True:
     # -------------------------- Country Search --------------------------
     if choice == "F":
         # display header title
-        clear()
-        title = " COUNTRY SEARCH "
-        print("="*70)
-        print("\033[1m{:^70}\033[0m".format(title))
-        print("="*70)
+        display_header(" COUNTRY SEARCH ")
 
         country_search = input("\nEnter the name of the country you want to find:\n> ").upper().strip()
         while not country_search:
@@ -129,30 +147,26 @@ while True:
         # loading screen to allow time for processing what is happening
         clear()
         print("\n\n\033[1;42m Loading. \033[0m\n\n")
-        time.sleep(0.2)
+        time.sleep(loading_time)
         clear()
         print("\n\n\033[1;42m Loading.. \033[0m\n\n")
-        time.sleep(0.2)
+        time.sleep(loading_time)
         clear()
         print("\n\n\033[1;42m Loading... \033[0m\n\n")
-        time.sleep(0.2)
+        time.sleep(loading_time)
 
         # search for inputed country
         data = ('%' + country_search + '%', '%' + country_search + '%', rule_name)
-        cursor.execute("SELECT country.*, traffic_rules.rule_name, traffic_rules.rule_value FROM country JOIN traffic_rules ON country.country_id = traffic_rules.country_id WHERE (UPPER(country.country_name) LIKE ? OR UPPER(country.country_code) LIKE ?) AND traffic_rules.rule_name = ?;", data)
+        cursor.execute(f"SELECT {country_columns} FROM country JOIN traffic_rules ON country.country_id = traffic_rules.country_id WHERE (UPPER(country.country_name) LIKE ? OR UPPER(country.country_code) LIKE ?) AND traffic_rules.rule_name = ?;", data)
         found_countries = cursor.fetchall()
-        header = ['\033[1mCOUNTRY ID\033[0m', '\033[1mCOUNTRY CODE\033[0m', '\033[1mNAME\033[0m', '\033[1mDRIVING SIDE\033[0m', '\033[1mMIN DRIVING AGE\033[0m', '\033[1mRULE NAME\033[0m', '\033[1mRULE VALUE\033[0m']
+        header = get_header()
 
         # print country data if found
         if found_countries:
             # display header title
-            clear()
-            title = " COUNTRIES FOUND: "
-            print("="*70)
-            print("\033[1m{:^70}\033[0m".format(title))
-            print("="*70)
-            formatted_rows = [(id, code, name, 'Left' if side else 'Right', age, rule_name, 'None' if rule_value == -1 else rule_value) for id, code, name, side, age, rule_name, rule_value in found_countries]
+            display_header(" COUNTRIES FOUND: ")
 
+            formatted_rows = get_format(found_countries)
             print(tabulate(formatted_rows, headers=header))
 
         # error if no matching found
@@ -165,11 +179,7 @@ while True:
     # -------------------------- Comparison Tool --------------------------
     elif choice == "C":
         # display header title
-        clear()
-        title = " COMPARISON TOOL "
-        print("="*70)
-        print("\033[1m{:^70}\033[0m".format(title))
-        print("="*70)
+        display_header(" COMPARISON TOOL ")
 
         # show the full table
         cursor.execute("SELECT * FROM country")
@@ -205,19 +215,26 @@ while True:
         else:
             rule_name = "Max Speed"
 
+        # loading screen to allow time for processing what is happening
+        clear()
+        print("\n\n\033[1;42m Loading. \033[0m\n\n")
+        time.sleep(loading_time)
+        clear()
+        print("\n\n\033[1;42m Loading.. \033[0m\n\n")
+        time.sleep(loading_time)
+        clear()
+        print("\n\n\033[1;42m Loading... \033[0m\n\n")
+        time.sleep(loading_time)
+
         # print the two countries
         data = [compare_id_1, compare_id_2, rule_name]
         cursor.execute("SELECT country.*, traffic_rules.rule_name, traffic_rules.rule_value FROM country JOIN traffic_rules ON country.country_id = traffic_rules.country_id WHERE (country.country_ID = ? OR country.country_ID = ?) AND rule_name = ?;", data)
-        output = cursor.fetchall()
+        compare_results = cursor.fetchall()
         header = ['\033[1mCOUNTRY ID\033[0m', '\033[1mCOUNTRY CODE\033[0m', '\033[1mNAME\033[0m', '\033[1mDRIVING SIDE\033[0m', '\033[1mMIN DRIVING AGE\033[0m', '\033[1mRULE NAME\033[0m', '\033[1mRULE VALUE\033[0m']
-        formatted_rows = [(id, code, name, 'Left' if side else 'Right', age, rule_name, 'None' if rule_value == -1 else rule_value) for id, code, name, side, age, rule_name, rule_value in output]
+        formatted_rows = [(id, code, name, 'Left' if side else 'Right', age, rule_name, 'None' if rule_value == -1 else rule_value) for id, code, name, side, age, rule_name, rule_value in compare_results]
 
         # display header title
-        clear()
-        title = " RESULTS "
-        print("\n" + "="*70)
-        print("\033[1m{:^70}\033[0m".format(title))
-        print("="*70)
+        display_header(" RESULTS ")
 
         print(tabulate(formatted_rows, headers=header, tablefmt="fancy_grid"))
 
@@ -227,11 +244,8 @@ while True:
     # -------------------------- Show Statistics --------------------------
     elif choice == "S":
         # display header title
-        clear()
-        title = " STATISTICS "
-        print("="*70)
-        print("\033[1m{:^70}\033[0m".format(title))
-        print("="*70)
+        display_header(" STATISTICS ")
+
         # retrieve information
         left_count = cursor.execute("SELECT COUNT(*) FROM country WHERE left_side=1").fetchall()
         right_count = cursor.execute("SELECT COUNT(*) FROM country WHERE left_side=0").fetchall()
@@ -245,14 +259,13 @@ while True:
         avg_alcohol_limit = cursor.execute("SELECT AVG(rule_value) FROM traffic_rules WHERE rule_name = 'Alcohol Limit' AND rule_value != -1").fetchall()
         avg_max_speed = cursor.execute("SELECT AVG(rule_value) FROM traffic_rules WHERE rule_name = 'Max Speed' AND rule_value != -1").fetchall()
         country_count = cursor.execute("SELECT COUNT(*) FROM country").fetchall()
-        last_updated = "26th of May 2025"
 
         # print information
         print("Driving Side Distribution:")
         print(f"Left\033[0m - \033[100m {left_count[0][0]} \033[0m | Right\033[0m - \033[100m {right_count[0][0]} \033[0m\n")
         print("Minimum Licence Distribution: ")
-        print(f"14yrs - \033[100m {min_licence_count_14[0][0] } countries\033[0m | 15yrs - \033[100m {min_licence_count_15[0][0]} countries \033[0m | 16yrs - \033[100m {min_licence_count_16[0][0]} countries \033[0m")
-        print(f"17yrs - \033[100m {min_licence_count_17[0][0] } countries\033[0m | 18yrs - \033[100m {min_licence_count_18[0][0]} countries \033[0m | 21yrs - \033[100m {min_licence_count_21[0][0]} countries \033[0m")
+        print(f"14yrs - \033[100m {min_licence_count_14[0][0]} countries \033[0m | 15yrs - \033[100m {min_licence_count_15[0][0]} countries \033[0m | 16yrs - \033[100m {min_licence_count_16[0][0]} countries \033[0m")
+        print(f"17yrs - \033[100m {min_licence_count_17[0][0]} countries \033[0m | 18yrs - \033[100m {min_licence_count_18[0][0]} countries \033[0m | 21yrs - \033[100m {min_licence_count_21[0][0]} countries \033[0m")
         print(f"\nAverage Minimum Driving Age:\033[0m \033[100m {round(avg_min_licence[0][0], 2)} \033[0m")
         print(f"Average Alcohol Limit:\033[0m \033[100m {round(avg_alcohol_limit[0][0], 2)} \033[0m")
         print(f"Average Max Speed:\033[0m \033[100m {round(avg_max_speed[0][0], 2)} \033[0m")
@@ -265,11 +278,7 @@ while True:
     # -------------------------- List all countries --------------------------
     elif choice == "L":
         # display header title
-        clear()
-        title = " COUNTRY LIST "
-        print("="*70)
-        print("\033[1m{:^70}\033[0m".format(title))
-        print("="*70 + "\n")
+        display_header(" COUNTRY LIST ")
 
         # get filter
         print("What do you want to filter by?")
@@ -293,11 +302,11 @@ while True:
 
         # get order
         print("\n\nWhat do you want the list ordered by?")
-        order_input = input("ID: \033[1;46m I \033[0m | Name: \033[1;46m N \033[0m | Driving Side: \033[1;46m D \033[0m | Minimum Licence Age: \033[1;46m M \033[0m | Alcohol Limit: \033[1;46m A \033[0m | Max Speed: \033[1;46m S \033[0m\n> ").upper().strip()
+        order_input = input("Name: \033[1;46m N \033[0m | Driving Side: \033[1;46m D \033[0m | Minimum Licence Age: \033[1;46m M \033[0m | Alcohol Limit: \033[1;46m A \033[0m | Max Speed: \033[1;46m S \033[0m\n> ").upper().strip()
         while not order_check(order_input):
-            print("\n\n\033[1;31mInvalid input. Please input either \033[1;46m I \033[0m\033[1;31m, \033[1;46m N \033[0m\033[1;31m, \033[1;46m D \033[0m\033[1;31m, \033[1;46m M \033[0m\033[1;31m, \033[1;46m A \033[0m\033[1;31m or \033[1;46m S \033[0m\n")
+            print("\n\n\033[1;31mInvalid input. Please input either \033[1;46m N \033[0m\033[1;31m, \033[1;46m D \033[0m\033[1;31m, \033[1;46m M \033[0m\033[1;31m, \033[1;46m A \033[0m\033[1;31m or \033[1;46m S \033[0m\n")
             print("\nWhat do you want the list ordered by?")
-            order_input = input("ID: \033[1;46m I \033[0m | Name: \033[1;46m N \033[0m | Driving Side: \033[1;46m D \033[0m | Minimum Licence Age: \033[1;46m M \033[0m | Alcohol Limit: \033[1;46m A \033[0m | Max Speed: \033[1;46m S \033[0m\n> ").upper().strip()
+            order_input = input("Name: \033[1;46m N \033[0m | Driving Side: \033[1;46m D \033[0m | Minimum Licence Age: \033[1;46m M \033[0m | Alcohol Limit: \033[1;46m A \033[0m | Max Speed: \033[1;46m S \033[0m\n> ").upper().strip()
 
         # ask for which rule to display if not ordered by rule
         if order_input != "A" and order_input != "S":
@@ -318,18 +327,16 @@ while True:
 
         # change filtering to inputted order
         if filter_input == "L":
-            filter_setting = True
-            filter = "1"
+            filter_toggle = True
+            filter_setting = "1"
         elif filter_input == "R":
-            filter_setting = True
-            filter = "0"
+            filter_toggle = True
+            filter_setting = "0"
         else:
-            filter_setting = False
+            filter_toggle = False
 
         # change ordering to inputted order
-        if order_input == "I":
-            order = "country_id"
-        elif order_input == "N":
+        if order_input == "N":
             order = "country_name"
         elif order_input == "M":
             order = "min_licence_age"
@@ -343,47 +350,43 @@ while True:
         # --- Filter System ---
         # connecting with traffic_rules.db database
         if order_input == "A" or order_input == "S":
-            if filter_setting is True:
-                data = [order, filter]
-                cursor.execute("SELECT country.*, traffic_rules.rule_name, traffic_rules.rule_value FROM country JOIN traffic_rules ON country.country_id = traffic_rules.country_id WHERE traffic_rules.rule_name = ? AND country.left_side = ? ORDER BY CAST(traffic_rules.rule_value AS FLOAT) ASC;", data)
+            if filter_toggle is True:
+                data = [order, filter_setting]
+                cursor.execute(f"SELECT {country_columns} FROM country JOIN traffic_rules ON country.country_id = traffic_rules.country_id WHERE traffic_rules.rule_name = ? AND country.left_side = ? ORDER BY CAST(traffic_rules.rule_value AS FLOAT) ASC;", data)
             else:
                 data = [order]
-                cursor.execute("SELECT country.*, traffic_rules.rule_name, traffic_rules.rule_value FROM country JOIN traffic_rules ON country.country_id = traffic_rules.country_id WHERE traffic_rules.rule_name = ? ORDER BY CAST(traffic_rules.rule_value AS FLOAT) ASC;", data)
-            header = ['\033[1mCOUNTRY ID\033[0m', '\033[1mCOUNTRY CODE\033[0m', '\033[1mNAME\033[0m', '\033[1mDRIVING SIDE\033[0m', '\033[1mMIN DRIVING AGE\033[0m', '\033[1mRULE NAME\033[0m', '\033[1mRULE VALUE\033[0m']
+                cursor.execute(f"SELECT {country_columns} FROM country JOIN traffic_rules ON country.country_id = traffic_rules.country_id WHERE traffic_rules.rule_name = ? ORDER BY CAST(traffic_rules.rule_value AS FLOAT) ASC;", data)
+            header = get_header()
             output = cursor.fetchall()
-            formatted_rows = [(id, code, name, 'Left' if side else 'Right', age, rule_name, 'None' if rule_value == -1 else rule_value) for id, code, name, side, age, rule_name, rule_value in output]
+            formatted_rows = get_format(output)
 
         # when not ordering by rule
         else:
-            header = ['\033[1mCOUNTRY ID\033[0m', '\033[1mCOUNTRY CODE\033[0m', '\033[1mNAME\033[0m', '\033[1mDRIVING SIDE\033[0m', '\033[1mMIN DRIVING AGE\033[0m', '\033[1mRULE NAME\033[0m', '\033[1mRULE VALUE\033[0m']
-            if filter_setting is False:
+            header = get_header()
+            if filter_toggle is False:
                 data = [rule_name]
-                cursor.execute(f"SELECT country.*, traffic_rules.rule_name, traffic_rules.rule_value FROM country JOIN traffic_rules ON country.country_id = traffic_rules.country_id WHERE traffic_rules.rule_name = ? ORDER BY {order};", data)
+                cursor.execute(f"SELECT {country_columns} FROM country JOIN traffic_rules ON country.country_id = traffic_rules.country_id WHERE traffic_rules.rule_name = ? ORDER BY {order};", data)
 
             else:
-                data = [rule_name, filter]
-                cursor.execute(f"SELECT country.*, traffic_rules.rule_name, traffic_rules.rule_value FROM country JOIN traffic_rules ON country.country_id = traffic_rules.country_id WHERE traffic_rules.rule_name = ? AND left_side = ? ORDER BY {order};", data)
+                data = [rule_name, filter_setting]
+                cursor.execute(f"SELECT {country_columns} FROM country JOIN traffic_rules ON country.country_id = traffic_rules.country_id WHERE traffic_rules.rule_name = ? AND left_side = ? ORDER BY {order};", data)
 
             output = cursor.fetchall()
-            formatted_rows = [(id, code, name, 'Left' if side else 'Right', age, rule_name, 'None' if rule_value == -1 else rule_value) for id, code, name, side, age, rule_name, rule_value in output]
+            formatted_rows = get_format(output)
 
         # loading screen to allow time for processing what is happening
         clear()
         print("\n\n\033[1;42m Loading. \033[0m\n\n")
-        time.sleep(0.2)
+        time.sleep(loading_time)
         clear()
         print("\n\n\033[1;42m Loading.. \033[0m\n\n")
-        time.sleep(0.2)
+        time.sleep(loading_time)
         clear()
         print("\n\n\033[1;42m Loading... \033[0m\n\n")
-        time.sleep(0.2)
+        time.sleep(loading_time)
 
         # display header title
-        clear()
-        title = " RESULTS: "
-        print("="*70)
-        print("\033[1m{:^70}\033[0m".format(title))
-        print("="*70)
+        display_header(" RESULTS: ")
 
         # print data (either fancy or simple)
         if style.lower() == "f":
@@ -398,5 +401,6 @@ while True:
     # end program when 'Q' is inputted
     elif choice == "Q":
         clear()
-        print("\n\n\033[1;42m  Goodbye! \033[0m\n\n")
-        break
+        print("\n\n\033[1;42m  Exiting Program. Goodbye! \033[0m\n\n")
+        connection.close()
+        sys.exit()
